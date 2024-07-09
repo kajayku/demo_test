fc63acea2ad8058e2c9ee44f46d0a139f92be738
ajay kushwaha <Ajay>
2024-07-09 18:59:43 +0530

    changes

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php 
 
-$conn = mysqli_connect("localhost","root","","php_crud") or die("Connection Faled!".mysqli_connect_error());
+$conn = mysqli_connect("localhost","root","","php_crud") or die("Connection Faled!".mysqli_connect_error()); 
 
 ?>
